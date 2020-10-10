d32c46e287727d3dd0460015239710d3fbc8f463
Greg Salomons <smokey.salomons@gmail.com>
2020-10-10 18:57:36 -0400

    Structure class tests working

--- Text Processing/test_sections.py
+++ Text Processing/test_sections.py
@@ -7,6 +7,7 @@ from typing import List
 from buffered_iterator import BufferedIterator
 import read_dvh_file
 from pprint import pprint
+import pandas as pd
 
 ##%% Test SectionBoundaries
 class TestSectionBoundaries(unittest.TestCase):
@@ -331,125 +332,229 @@ class TestProcessing(unittest.TestCase):
         self.assertListEqual(test_output, self.merged_line_output)
 
 
-#%% Test Text
-test_source_groups = [
-    [
-        ['Patient Name         ', ' ____, ____'],
-        ['Patient ID           ', ' 1234567'],
-        ['Comment              ', ' DVHs for multiple plans and plan sums'],
-        ['Date                 ', ' Friday, January 17, 2020 09:45:07'],
-        ['Exported by          ', ' gsal'],
-        ['Type                 ', ' Cumulative Dose Volume Histogram'],
-        ['Description          ', ' The cumulative DVH displays the percentage (relative)'],
-        ['    '],
-        [                          'or volume (absolute) of structures that receive a dose'],
-        [                          'equal to or greater than a given dose.']
-        ], (
-    'Patient Name         : ____, ____\r\n'
-    'Patient ID           : 1234567\r\n'
-    'Comment              : DVHs for multiple plans and plan sums\r\n'
-    'Date                 : Friday, January 17, 2020 09:45:07\r\n'
-    'Exported by          : gsal\r\n'
-    'Type                 : Cumulative Dose Volume Histogram\r\n'
-    'Description          : The cumulative DVH displays the percentage (relative)\r\n'
-    '                       or volume (absolute) of structures that receive a dose\r\n'
-    '                       equal to or greater than a given dose.\r\n'
-    '\r\n'
-    'Plan sum: Plan Sum\r\n'
-    'Course: PLAN SUM\r\n'
-    ), (
-    'Plan sum: Plan Sum\r\n'
-    'Course: PLAN SUM\r\n'
-    'Prescribed dose [cGy]: not defined\r\n'
-    '% for dose (%): not defined\r\n'
-    '\r\n'
-    'Plan: PARR\r\n'
-    ), (
-    'Plan: PARR\r\n'
-    'Course: C1\r\n'
-    'Plan Status: Treatment Approved Thursday, January 02, 2020 12:55:56 by gsal\r\n'
-    'Prescribed dose [cGy]: 5000.0\r\n'
-    '% for dose (%): 100.0\r\n'
-    '\r\n'
-    'Structure: PRV5 SpinalCanal\r\n'
-    )]
-
-test_result_dicts = [
-    [
-        ['Patient Name', '____, ____'],
-        ['Patient ID ', '1234567'],
-        ['Comment', 'DVHs for multiple plans and plan sums'],
-        ['Date', ' Friday, January 17, 2020 09:45:07'],
-        ['Exported by', 'gsal'],
-        ['Type', 'Cumulative Dose Volume Histogram'],
-        ['Description', ('The cumulative DVH displays the percentage (relative)\n'
-                         'or volume (absolute) of structures that receive a dose\n'
-                         'equal to or greater than a given dose.')]
-        ],
-    {'Patient Name': '____, ____',
-     'Patient ID': '1234567',
-     'Comment': 'DVHs for multiple plans and plan sums',
-     'Date': 'Friday, January 17, 2020 09:45:07',
-     'Exported by': 'gsal',
-     'Type': 'Cumulative Dose Volume Histogram',
-     'Description': ('The cumulative DVH displays the percentage (relative) '
-                     'or volume (absolute) of structures that receive a dose '
-                     'equal to or greater than a given dose.')
-     },
-    {'Plan sum': 'Plan Sum',
-     'Course': 'PLAN SUM',
-     'Prescribed dose': '',
-     'Prescribed dose Unit': '',
-     '% for dose (%)': 'not defined'
-     },
-    {'Plan': 'PARR',
-     'Course': 'C1',
-     'Plan Status': 'Treatment Approved',
-     'Approved on': 'Thursday, January 02, 2020 12:55:56',
-     'Approved by': 'gsal',
-     'Prescribed dose': 5000.0,
-     'Prescribed dose Unit': 'cGy',
-     '% for dose (%)': 100.0}
-     ]
-
-
 class TestSections(unittest.TestCase):
     def setUp(self):
+        self.test_source = {
+            'DVH Info': [
+                'Patient Name         : ____, ____',
+                'Patient ID           : 1234567',
+                'Comment              : DVHs for multiple plans and plan sums',
+                'Date                 : Friday, January 17, 2020 09:45:07',
+                'Exported by          : gsal',
+                'Type                 : Cumulative Dose Volume Histogram',
+                ('Description          : The cumulative DVH displays the '
+                'percentage (relative)'),
+                ('                       or volume (absolute) of structures '
+                'that receive a dose'),
+                '                      equal to or greater than a given dose.',
+                ''
+                ],
+            'Plan Info 1': [
+                'Plan sum: Plan Sum',
+                'Course: PLAN SUM',
+                'Prescribed dose [cGy]: not defined',
+                '% for dose (%): not defined',
+                ''
+                ],
+            'Plan Info 2': [
+                'Plan: PARR',
+                'Course: C1',
+                'Plan Status: Treatment Approved Thursday, January 02, '
+                '2020 12:55:56 by gsal',
+                'Prescribed dose [cGy]: 5000.0',
+                '% for dose (%): 100.0',
+                ''
+                ],
+            'Structure': [
+                'Structure: PRV5 SpinalCanal',
+                'Approval Status: Approved',
+                'Plan: Plan Sum',
+                'Course: PLAN SUM',
+                'Volume [cm³]: 121.5',
+                'Dose Cover.[%]: 100.0',
+                'Sampling Cover.[%]: 100.1',
+                'Min Dose [cGy]: 36.7',
+                'Max Dose [cGy]: 3670.1',
+                'Mean Dose [cGy]: 891.9',
+                'Modal Dose [cGy]: 44.5',
+                'Median Dose [cGy]: 863.2',
+                'STD [cGy]: 621.9',
+                'NDR: ',
+                'Equiv. Sphere Diam. [cm]: 6.1',
+                'Conformity Index: N/A',
+                'Gradient Measure [cm]: N/A',
+                ''
+                ],
+            'DVH': [
+                'Dose [cGy] Ratio of Total Structure Volume [%]',
+                '         0                       100',
+                '         1                       100',
+                '         2                       100',
+                '         3                       100',
+                '         4                       100',
+                '         5                       100',
+                '      3667              4.23876e-005',
+                '      3668              2.87336e-005',
+                '      3669              1.50797e-005',
+                '      3670               1.4257e-006',
+                ]
+            }
+
+        self.test_result = {
+            'DVH Info': {
+                    'Patient Name': '____, ____',
+                    'Patient ID': '1234567',
+                    'Comment': 'DVHs for multiple plans and plan sums',
+                    'Date': 'Friday, January 17, 2020 09:45:07',
+                    'Exported by': 'gsal',
+                    'Type': 'Cumulative Dose Volume Histogram',
+                    'Description': ('The cumulative DVH displays the '
+                                    'percentage (relative) or volume '
+                                    '(absolute) of structures that receive a '
+                                    'dose equal to or greater than a '
+                                    'given dose.')
+                    },
+            'Plan Info 1': {
+                    'Plan sum': 'Plan Sum',
+                    'Course': 'PLAN SUM',
+                    'Prescribed dose': '',
+                    'Prescribed dose unit': '',
+                    '% for dose (%)': 'not defined'
+                    },
+            'Plan Info 2': {
+                    'Plan': 'PARR',
+                    'Course': 'C1',
+                    'Plan Status': 'Treatment Approved',
+                    'Approved on': 'Thursday, January 02, 2020 12:55:56',
+                    'Approved by': 'gsal',
+                    'Prescribed dose': 5000.0,
+                    'Prescribed dose unit': 'cGy',
+                    '% for dose (%)': 100.0
+                    },
+            'Structure': {
+                    'Structure': 'PRV5 SpinalCanal',
+                    'Approval Status': 'Approved',
+                    'Plan': 'Plan Sum',
+                    'Course': 'PLAN SUM',
+                    'Volume [cc]': 121.5,
+                    'Dose Cover.[%]': 100.0,
+                    'Sampling Cover.[%]': 100.1,
+                    'Min Dose [cGy]': 36.7,
+                    'Max Dose [cGy]': 3670.1,
+                    'Mean Dose [cGy]': 891.9,
+                    'Modal Dose [cGy]': 44.5,
+                    'Median Dose [cGy]': 863.2,
+                    'STD [cGy]': 621.9,
+                    'NDR': '',
+                    'Equiv. Sphere Diam. [cm]': 6.1,
+                    'Conformity Index': 'N/A',
+                    'Gradient Measure [cm]': 'N/A'
+                    },
+            'DVH': pd.DataFrame({'Dose [cGy]': [0, 1, 2, 3, 4, 5, 3667, 3668,
+                                                3669, 3670],
+                'Ratio of Total Structure Volume [%]': [100, 100, 100, 100,
+                                                        100, 100,
+                                                        4.23876e-005,
+                                                        2.87336e-005,
+                                                        1.50797e-005,
+                                                        1.4257e-006]})
+            }
+
         self.context = {
             'File Name': 'trigger_test_text.txt',
             'File Path': Path.cwd() / 'trigger_test_text.txt',
             'Line Count': 0
             }
-        preprocessing_methods = [clean_ascii_text]
-        parsing_rules = [read_dvh_file.make_date_parse_rule()]
-        default_parser = read_dvh_file.make_default_csv_parser()
-        line_parser = tp.LineParser(parsing_rules, default_parser)
-        post_processing_methods=[
-            tp.trim_items,
-            tp.drop_blanks,
-            tp.merge_continued_rows
-            ],
+        self.default_parser = tp.define_csv_parser('dvh_info', delimiter=':',
+                                                   skipinitialspace=True)
+
+    def test_dvh_info_section(self):
+        dvh_info_section = tp.Section(
+            section_name='DVH Info',
+            preprocessing_methods=[clean_ascii_text],
+            parsing_rules=[read_dvh_file.make_date_parse_rule()],
+            default_parser=self.default_parser,
+            post_processing_methods=[tp.trim_items, tp.drop_blanks,
+                                     tp.merge_continued_rows],
+            output_method=tp.to_dict)
+        # scan_section
+        source = BufferedIterator(self.test_source['DVH Info'])
+        self.context['Source'] = source
+        test_output = dvh_info_section.scan_section(source, self.context)
+
+        self.assertDictEqual(test_output, self.test_result['DVH Info'])
+
+    def test_plan_info1_section(self):
+        dvh_info_section = tp.Section(
+            section_name='Plan Info 1',
+            preprocessing_methods=[clean_ascii_text],
+            parsing_rules=[read_dvh_file.make_prescribed_dose_rule(),
+                           read_dvh_file.make_approved_status_rule()],
+            default_parser=self.default_parser,
+            post_processing_methods=[tp.trim_items, tp.drop_blanks,
+                                     tp.convert_numbers],
+            output_method=tp.to_dict)
+        # scan_section
+        source = BufferedIterator(self.test_source['Plan Info 1'])
+        self.context['Source'] = source
+        test_output = dvh_info_section.scan_section(source, self.context)
+
+        self.assertDictEqual(test_output, self.test_result['Plan Info 1'])
 
 
-        self.dvh_info_section = tp.Section(
-            section_name='dvh_info',
-            preprocessing=[clean_ascii_text],
-            parsing_rules=parsing_rules,
-            processed_lines=[
-                tp.trim_items,
-                tp.drop_blanks,
-                tp.merge_continued_rows
-                ],
-            output_method=tp.to_dict
-            )
+    def test_plan_info2_section(self):
+        dvh_info_section = tp.Section(
+            section_name='Plan Info 2',
+            preprocessing_methods=[clean_ascii_text],
+            parsing_rules=[read_dvh_file.make_prescribed_dose_rule(),
+                           read_dvh_file.make_approved_status_rule()],
+            default_parser=self.default_parser,
+            post_processing_methods=[tp.trim_items, tp.drop_blanks,
+                                     tp.convert_numbers],
+            output_method=tp.to_dict)
+        # scan_section
+        source = BufferedIterator(self.test_source['Plan Info 2'])
+        self.context['Source'] = source
+        test_output = dvh_info_section.scan_section(source, self.context)
+
+        self.assertDictEqual(test_output, self.test_result['Plan Info 2'])
+
+
+    def test_structure_section(self):
+        dvh_info_section = tp.Section(
+            section_name='Structure',
+            preprocessing_methods=[clean_ascii_text],
+            parsing_rules=[],
+            default_parser=self.default_parser,
+            post_processing_methods=[tp.trim_items, tp.drop_blanks,
+                                     tp.convert_numbers],
+            output_method=tp.to_dict)
+        # scan_section
+        source = BufferedIterator(self.test_source['Structure'])
+        self.context['Source'] = source
+        test_output = dvh_info_section.scan_section(source, self.context)
+
+        self.assertDictEqual(test_output, self.test_result['Structure'])
 
 
     @unittest.skip('Not Working')
-    def test_dvh_info_section(self):
-        source = (line for line in test_source_groups[1].splitlines())
+    def test_dvh_section(self):
+        dvh_info_section = tp.Section(
+            section_name='DVH',
+            preprocessing_methods=[clean_ascii_text],
+            parsing_rules=[read_dvh_file.make_date_parse_rule()],
+            default_parser=self.default_parser,
+            post_processing_methods=[tp.trim_items, tp.drop_blanks,
+                                     tp.merge_continued_rows],
+            output_method=tp.to_dict)
+        # scan_section
+        source = BufferedIterator(self.test_source['DVH'])
         self.context['Source'] = source
-        section_output = self.dvh_info_section.scan_section(self.context)
-        self.assertDictEqual(section_output, test_result_dicts[1])
+        test_output = dvh_info_section.scan_section(source, self.context)
+
+        self.assertDictEqual(test_output, self.test_result['DVH'])
+
+
 
 
 if __name__ == '__main__':
